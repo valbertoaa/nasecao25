967c31199c12dec81fd38390e5129630bfb54f2a
Valberto Alves <valberto.alvesagostinho@gmail.com>
2022-02-06 20:43:59 -0300

    Projeto Criado Seção 25 Prof. Nélio Alves

---
+++ src/main/java/br/com/essencial/nasecao25/Nasecao25Application.java
@@ -0,0 +1,13 @@
+package br.com.essencial.nasecao25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nasecao25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nasecao25Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/essencial/aula01/Nasecao25ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.essencial.aula01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nasecao25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
